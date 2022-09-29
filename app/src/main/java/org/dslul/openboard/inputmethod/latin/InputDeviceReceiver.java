ecd0beee6ef45ccb5983f623e3df61b8efc47492
noernova <noernova666@gmail.com>
2022-09-29 23:54:42 +0700

    support Physical keyboard

---
+++ app/src/main/java/org/dslul/openboard/inputmethod/latin/InputDeviceReceiver.java
@@ -0,0 +1,12 @@
+package org.dslul.openboard.inputmethod.latin;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class InputDeviceReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
